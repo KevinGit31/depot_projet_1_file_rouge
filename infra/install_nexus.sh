3723ef9518cb61537440897edd4dd4e4958207b5
gb-0001 <gb-0001@outlook.fr>
2021-07-08 13:54:16 +0200

    MAJ instance.yaml + script install nexus

--- infra/install_nexus.sh
+++ infra/install_nexus.sh
@@ -1,79 +1,114 @@
 #!/usr/bin/bash
 
-### MODE SECURE
-set -u # en cas de variable non définit, arreter le script
-set -e # en cas d'erreur (code de retour non-zero) arreter le script
 
-ps_assert_root(){
-	REAL_ID="$(id -u)"
-	if [ "$REAL_ID" -ne 0 ]; then
-		1>&2 echo "ERREUR: Le script doit etre exécuté en tant que root"
-		exit 1
-	fi
+#Execution du script en tant que root
+root_connect(){
+    ID="$(id -u)"
+    if ["$ID" -ne 0]; then
+        1>&2 echo "Vous devez executer le script en tant que root"
+        exit 1
+    fi
 }
 
-# debut du lancement
+#Installation des packages sous forme de fonction avec vérification si le package est installé
+install_package() {
+    PACKAGE="$1"
+    if ! dpkg -l |grep --quiet "^ii.$PACKAGE"; then
+        sudo apt install -y "$PACKAGE"
+    fi
+}
 
-ps_assert_root
+#Test si openjdk est installé
+dpkg -l | grep openjdk-8-jdk
+RETOUR_INSTALL_OPENJDK=$?
+#Test presence du binaire nexus
+RETOUR_BIN_NEXUS=/opt/nexus/bin/nexus
 
-apt update -y
-apt install -y openjdk-8-jdk
+if [[ $RETOUR_INSTALL_OPENJDK = 0 ]] && [[ ! -f $RETOUR_BIN_NEXUS ]]; then
+    echo "Serveur Nexus déjà installé"
+    exit 0
+fi
 
-useradd -M -d /opt/nexus -s /bin/bash -r nexus
-echo "nexus   ALL=(ALL)       NOPASSWD: ALL" > /etc/sudoers.d/nexus
+sudo apt -y update
 
-wget https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz
+install_package openjdk-8-jdk
+#Test si le user nexus existe
+RETOUR_USER_NEXUS=sudo cat /etc/passwd | grep -i nexus
+echo $RETOUR_USER_NEXUS
+#creation du user nexus et positionnement dans le sudoers
+if [[ $RETOUR_USER_NEXUS != 0 ]]; then
+    sudo useradd -M -d /opt/nexus -s /bin/bash -r nexus
+    sudo echo 'nexus   ALL=(ALL)       NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
+fi
 
-mkdir /opt/nexus
+#Download des sources
+URLDL=https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.31.1-01-unix.tar.gz
+wget $URLDL
+sleep 5s
 
-tar xzf nexus-3.29.2-02-unix.tar.gz -C /opt/nexus --strip-components=1
+if [[ ! -f nexus-3.31.1-01-unix.tar.gz ]]; then
+    echo "Problème de download $URLDL \n"
+    exit 0
+fi
 
-chown -R nexus: /opt/nexus
+#creation du dossier cible
+if [ ! -d "/opt/nexus" ]; then
+        sudo mkdir /opt/nexus
+        #decompression des sources dans le dossier cible
+        sudo tar xzf nexus-3.31.1-01-unix.tar.gz -C /opt/nexus --strip-components=1
+        #positionnement du proprietaire dans le dossier cible
+        sudo chown -R nexus: /opt/nexus
+fi
 
-sed -i 's/#run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
+#positionnement du user nexus
+grep "run_as_user=\"nexus\"" /opt/nexus/bin/nexus.rc
+if [[ ! $? = 0 ]]; then
+    sudo sed -i 's/#run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
+fi
 
-## On modifie la configuration pour qu'elle fonctionne
+## Ajustement des chemins
 sudo sed -i 's/..\/sonatype-work/.\/sonatype-work/g' /opt/nexus/bin/nexus.vmoptions
-sudo sed -i 's/2073/1024/g' /opt/nexus/bin/nexus.vmoptions
+sudo sed -i 's/2703/1024/g' /opt/nexus/bin/nexus.vmoptions
 
-sudo -u nexus /opt/nexus/bin/nexus start
-
-
-# Service
-
-cat > /etc/systemd/system/nexus.service << 'EOL'
+# Creation du serviceService
+sudo bash -c "cat > /etc/systemd/system/nexus.service" <<EOF
 [Unit]
 Description=nexus service
 After=network.target
+
 [Service]
 Type=forking
 LimitNOFILE=65536
 ExecStart=/opt/nexus/bin/nexus start
 ExecStop=/opt/nexus/bin/nexus stop
 User=nexus
+Group=nexus
 Restart=on-abort
+
 [Install]
 WantedBy=multi-user.target
-EOL
+EOF
 
-/opt/nexus/bin/nexus stop
 
-systemctl daemon-reload
+#Demarrage de nexus
+sudo systemctl enable nexus
 
-systemctl enable --now nexus.service
+sudo systemctl start nexus
 
-ufw allow 8081/tcp
 
+#Autorisation de l'acces
+sudo ufw allow 8081/tcp
 
 #install fail2ban
-apt install -y fail2ban
+install_package "fail2ban"
 
 #start du service fail2ban + activation au démarrage
-systemctl start fail2ban
-systemctl enable fail2ban
+sudo systemctl start fail2ban
+sudo systemctl enable fail2ban
 
 #configuration fail2ban
-echo "[DEFAULT]
+sudo bash -c "cat >> /etc/fail2ban/jail.d/custom.conf" <<EOF
+[DEFAULT]
 ignoreip = 127.0.0.1
 findtime = 10m
 bantime = 24h
@@ -82,19 +117,14 @@ maxretry = 3
 [sshd]
 enabled = true
 logpath = /var/log/auth.log
+EOF
 
-" >> /etc/fail2ban/jail.d/custom.conf
+sudo systemctl restart fail2ban
 
-systemctl restart fail2ban
-
-
-sleep 300s
 
 # Affiche le mot de passe
-echo 'Mot de passe admin \n'
-cat /opt/nexus/sonatype-work/nexus3/admin.password
+echo 'Mot de passe admin dans 40s en cours de generation PATIENTEZ SVP...:\n'
+#Attente pour la generation du password
+sleep 40s
+sudo cat /opt/nexus/sonatype-work/nexus3/admin.password
 echo '\n\n'
-
-echo "Success"
-
-
