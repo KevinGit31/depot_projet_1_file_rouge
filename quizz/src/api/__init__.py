92606dfffd720d9a6e5d9fd91d738fc64248bfdf
Gerard Fevill <nouglozeh_gerard@yahoo.fr>
2021-07-09 11:46:44 +0200

    Réponse à 55%

--- quizz/src/api/__init__.py
+++ quizz/src/api/__init__.py
@@ -4,7 +4,9 @@ from flask_marshmallow import Marshmallow
 from models.init_helper import helper
 from config.appconfig import app_config
 # Init helper
-app = Flask(__name__)
+app = Flask(__name__,
+template_folder="../templates",
+    static_folder="../static")
 
 # Database
 app_config(app)
--- quizz/src/api/app.py
+++ quizz/src/api/app.py
@@ -6,10 +6,11 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 from controllers.controller_answer import controller_answer
+from routes.routes import configure_routes
 from api.__init__ import app
 
-
 controller_answer(app)
+configure_routes(app)
 
 # Run Server
 if __name__ == '__main__':
