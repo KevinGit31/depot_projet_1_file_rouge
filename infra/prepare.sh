ed38fb883da577555fb67f63484f72c13cfde4c1
gb-0001 <gb-0001@outlook.fr>
2021-07-16 19:43:44 +0200

    MAJ script de preparation

--- infra/prepare.sh
+++ infra/prepare.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [[ !Ref Urlscript == "jenkins" ]]; then
-CMDSCRIPT1=$(sudo wget -O /tmp/install_jenkins.sh https://raw.githubusercontent.com/KevinGit31/depot_projet_1_file_rouge/dev/infra/install_jenkins.sh && chmod +x /tmp/install_jenkins.sh && sudo /bin/bash /tmp/install_jenkins.sh)
-elif [[ !Ref Urlscript == "nexus" ]]; then
+HNAME=$(echo hostname | awk 1 ORS='' )
+if [[ -n "$HNAME | grep \"jenkins\"" ]]; then
+    CMDSCRIPT1=$(sudo wget -O /tmp/install_jenkins.sh https://raw.githubusercontent.com/KevinGit31/depot_projet_1_file_rouge/dev/infra/install_jenkins.sh && chmod +x /tmp/install_jenkins.sh && sudo /bin/bash /tmp/install_jenkins.sh)
+elif [[ -n "$HNAME | grep \"nexus\"" ]]; then
 CMDSCRIPT2=$(sudo wget -O /tmp/install_nexus.sh https://raw.githubusercontent.com/KevinGit31/depot_projet_1_file_rouge/dev/infra/install_nexus.sh && chmod +x /tmp/install_nexus.sh && sudo /bin/bash /tmp/install_nexus.sh)
-elif [[ !Ref Urlscript == "envapp" ]]; then
-CMDSCRIPT3=$(sudo wget -O /tmp/install_envapp.sh https://raw.githubusercontent.com/KevinGit31/depot_projet_1_file_rouge/dev/infra/install_envapp.sh && chmod +x /tmp/install_envapp.sh && sudo /bin/bash /tmp/install_envapp.sh)
+elif [[ -n "$HNAME | grep \"qcm\"" ]]; then
+CMDSCRIPT3=$(sudo wget -O /tmp/install_qcm.sh https://raw.githubusercontent.com/KevinGit31/depot_projet_1_file_rouge/dev/infra/install_qcm.sh && chmod +x /tmp/install_qcm.sh && sudo /bin/bash /tmp/install_qcm.sh)
 fi
