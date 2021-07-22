2d6bf30cb2f70f0dd21d307272132b5107c787ba
gb-0001 <gb-0001@outlook.fr>
2021-07-22 11:25:59 +0200

    MAJ inst jenkins

--- infra/install_jenkins.sh
+++ infra/install_jenkins.sh
@@ -4,6 +4,24 @@ JENKINSPWD=$(cat /tmp/jenkinskey.txt)
 DEVOPSPWD=$(cat /tmp/devopsuserkey.txt)
 ROOTPASS=$(cat /tmp/root.txt)
 ANSIBPASS=$(cat /tmp/ansiblekey.txt)
+ENV1=$(cat /tmp/env.txt)
+KEYNAME1=$(cat /tmp/KeyName.txt)
+REGION1=$(cat /tmp/region.txt)
+SUBIDPUB1=$(cat /tmp/SubnetIdPub.txt)
+SUBIDPRIV1=$(cat /tmp/SubnetIdPriv.txt)
+VPCID1=$(cat /tmp/VpcId.txt)
+PRIVIP1=$(cat /tmp/PrivateIP.txt)
+INGRPORT=$(cat /tmp/IngressPort.txt)
+SECGRPNLST=$(cat /tmp/SecurityGroupNameList.txt)
+USCRIPT=$(cat /tmp/Urlscript.txt)
+SECGRPID=$(cat /tmp/SecurityGroupId.txt)
+INSTTYPE=$(cat /tmp/InstanceType.txt)
+AAKI1=$(cat /tmp/AWSAccessKeyId.txt)
+ASAKI1=$(cat /tmp/AWSSecretAccessKeyId.txt)
+TYPENAME1=$(cat /tmp/TypeName.txt)
+
+
+
 #install java
 yum install -y java-1.8.0-openjdk-devel
 #recuperation package
@@ -60,6 +78,33 @@ echo 'devops   ALL=(ALL)       NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 echo "root:$ROOTPASS" | chpasswd
 #genere la cle pub et priv pour le user devops
 #su - devops -c 'ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1 ; exit'
+
+
+su - userjenkins -c "mkdir ~/.aws && cd ~/.aws && echo \"[default]\" >> credentials && echo \"aws_access_key_id=$AAKI1\" >> credentials && echo \"aws_secret_access_key=$ASAKI1\" >> credentials && exit"
+su - userjenkins -c "cd ~/.aws && echo \"[default]\" >> config && echo \"region=$REGION1\" >> config && echo \"output=json\" >> config && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $DEVOPSPWD --name \"secret_devops\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $ROOTPASS --name \"ROOTKEY\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $KEYNAME1 --name \"KEYNAME\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $TYPENAME1 --name \"TypeName\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $REGION1 --name \"REGION\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $SUBIDPUB1 --name \"SubnetIdPub\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $SUBIDPRIV1 --name \"SubnetIdPriv\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $VPCID1 --name \"VpcId\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $PRIVIP1 --name \"PrivateIP\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $INGRPORT --name \"IngressPort\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $SECGRPNLST --name \"SecurityGroupNameList\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $USCRIPT --name \"Urlscript\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $SECGRPID --name \"SecurityGroupId\" >> all && exit"
+su - userjenkins -c "cd /home/userjenkin && ansible-vault encrypt_string $INSTTYPE --name \"InstanceType\" >> all && exit"
+if [[ $ENV1 = "main" ]]; then
+    CONVENV=prod
+    su - userjenkins -c "cd /home/userjenkin/depot_projet_1_file_rouge/infra/ansible/inventory/$CONVENV/group_vars/all && ansible-vault encrypt_string $CONVENV --name \"ENVIRONNEMENT\" >> all && exit"
+else
+    su - userjenkins -c "cd /home/userjenkin/depot_projet_1_file_rouge/infra/ansible/inventory/dev/group_vars/all && ansible-vault encrypt_string $ENV1 --name \"ENVIRONNEMENT\" >> all && exit"
+fi
+su - userjenkins -c "yes | cp -rf /home/userjenkin/all /home/userjenkin/depot_projet_1_file_rouge/infra/ansible/inventory/dev/group_vars/all/all"
+su - userjenkins -c "yes | cp -rf /home/userjenkin/all /home/userjenkin/depot_projet_1_file_rouge/infra/ansible/inventory/qua/group_vars/all/all"
+su - userjenkins -c "yes | cp -rf /home/userjenkin/all /home/userjenkin/depot_projet_1_file_rouge/infra/ansible/inventory/prod/group_vars/all/all"
 
 
 #Nettoyage /tmp
