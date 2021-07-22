3325bded764039288c30a68a7608ecb6724219e6
gb-0001 <gb-0001@outlook.fr>
2021-07-22 18:11:54 +0200

    MAJ ANsible playbook mise en place Variable d'environnement

--- infra/install_jenkins.sh
+++ infra/install_jenkins.sh
@@ -11,11 +11,11 @@ SUBIDPUB1=$(cat /tmp/SubnetIdPub.txt)
 SUBIDPRIV1=$(cat /tmp/SubnetIdPriv.txt)
 VPCID1=$(cat /tmp/VpcId.txt)
 PRIVIP1=$(cat /tmp/PrivateIP.txt)
-INGRPORT=$(cat /tmp/IngressPort.txt)
-SECGRPNLST=$(cat /tmp/SecurityGroupNameList.txt)
-USCRIPT=$(cat /tmp/Urlscript.txt)
-SECGRPID=$(cat /tmp/SecurityGroupId.txt)
-INSTTYPE=$(cat /tmp/InstanceType.txt)
+INGRPORT1=$(cat /tmp/IngressPort.txt)
+SECGRPNLST1=$(cat /tmp/SecurityGroupNameList.txt)
+USCRIPT1=$(cat /tmp/Urlscript.txt)
+SECGRPID1=$(cat /tmp/SecurityGroupId.txt)
+INSTTYPE1=$(cat /tmp/InstanceType.txt)
 AAKI1=$(cat /tmp/AWSAccessKeyId.txt)
 ASAKI1=$(cat /tmp/AWSSecretAccessKeyId.txt)
 TYPENAME1=$(cat /tmp/TypeName.txt)
@@ -57,6 +57,7 @@ useradd -m userjenkins
 echo "userjenkins:$JENKINSPWD" | sudo chpasswd
 echo 'userjenkins   ALL=(ALL)       NOPASSWD: ALL' | sudo EDITOR='tee -a' visudo
 usermod -a -G jenkins userjenkins
+usermod -a -G userjenkins jenkins
 #sudo -H -u userjenkins -c 'mkdir -p ~/ansible'
 #su - userjenkins -c 'git clone https://github.com/KevinGit31/depot_projet_1_file_rouge.git ; exit'
 
@@ -80,28 +81,50 @@ echo "root:$ROOTPASS" | chpasswd
 #su - devops -c 'ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1 ; exit'
 
 
-su - userjenkins -c "cd /home/userjenkins && wget -O $ENV1.zip https://github.com/KevinGit31/depot_projet_1_file_rouge/archive/refs/heads/$ENV1.zip && unzip $ENV1.zip && mv depot_projet_1_file_rouge* depot_projet_1_file_rouge && chmod +x /home/userjenkins/depot_projet_1_file_rouge/infra/ansvlt.sh && exit"
+#su - userjenkins -c "cd /home/userjenkins && wget -O $ENV1.zip https://github.com/KevinGit31/depot_projet_1_file_rouge/archive/refs/heads/$ENV1.zip && unzip $ENV1.zip && mv depot_projet_1_file_rouge* depot_projet_1_file_rouge && chmod +x /home/userjenkins/depot_projet_1_file_rouge/infra/ansvlt.sh && exit"
 su - devops -c "cd /tmp && /bin/bash ssh.sh && exit"
 su - userjenkins -c "mkdir ~/.aws && cd ~/.aws && echo \"[default]\" >> credentials && echo \"aws_access_key_id=$AAKI1\" >> credentials && echo \"aws_secret_access_key=$ASAKI1\" >> credentials && exit"
 su - userjenkins -c "cd ~/.aws && echo \"[default]\" >> config && echo \"region=$REGION1\" >> config && echo \"output=json\" >> config && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $DEVOPSPWD --name \"secret_devops\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $ROOTPASS --name \"ROOTKEY\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $KEYNAME1 --name \"KEYNAME\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $TYPENAME1 --name \"TypeName\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $REGION1 --name \"REGION\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $SUBIDPUB1 --name \"SubnetIdPub\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $SUBIDPRIV1 --name \"SubnetIdPriv\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $VPCID1 --name \"VpcId\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $PRIVIP1 --name \"PrivateIP\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $INGRPORT --name \"IngressPort\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $SECGRPNLST --name \"SecurityGroupNameList\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $USCRIPT --name \"Urlscript\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $SECGRPID --name \"SecurityGroupId\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $INSTTYPE --name \"InstanceType\" >> all && exit"
-su - userjenkins -c "cd /home/userjenkins && cat /home/userjenkins/all >> /home/userjenkins/depot_projet_1_file_rouge/infra/ansible/inventory/dev/group_vars/all/all"
-su - userjenkins -c "cd /home/userjenkins && cat /home/userjenkins/all >> /home/userjenkins/depot_projet_1_file_rouge/infra/ansible/inventory/qua/group_vars/all/all"
-su - userjenkins -c "cd /home/userjenkins && cat /home/userjenkins/all >> /home/userjenkins/depot_projet_1_file_rouge/infra/ansible/inventory/prod/group_vars/all/all"
+su - userjenkins -c "echo \"export SECRETDEVOPS=$DEVOPSPWD\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $DEVOPSPWD --name \"secret_devops\" >> all && exit"
+su - userjenkins -c "echo \"export ROOTKEY=$ROOTPASS\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $ROOTPASS --name \"ROOTKEY\" >> all && exit"
+su - userjenkins -c "echo \"export KEYNAME=$KEYNAME1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $KEYNAME1 --name \"KEYNAME\" >> all && exit"
+su - userjenkins -c "echo \"export TYPENAME=$TYPENAME1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $TYPENAME1 --name \"TypeName\" >> all && exit"
+su - userjenkins -c "echo \"export REGION1=$REGION\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $REGION1 --name \"REGION\" >> all && exit"
+su - userjenkins -c "echo \"export SUBIDPUB=$SUBIDPUB1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $SUBIDPUB1 --name \"SubnetIdPub\" >> all && exit"
+su - userjenkins -c "echo \"export SUBIDPRIV=$SUBIDPRIV1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $SUBIDPRIV1 --name \"SubnetIdPriv\" >> all && exit"
+su - userjenkins -c "echo \"export VPCID=$VPCID1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $VPCID1 --name \"VpcId\" >> all && exit"
+su - userjenkins -c "echo \"export PRIVIP=$PRIVIP1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $PRIVIP1 --name \"PrivateIP\" >> all && exit"
+su - userjenkins -c "echo \"export INGRPORT=$INGRPORT1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $INGRPORT --name \"IngressPort\" >> all && exit"
+su - userjenkins -c "echo \"export SECGRPNLST=$SECGRPNLST\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $SECGRPNLST --name \"SecurityGroupNameList\" >> all && exit"
+su - userjenkins -c "echo \"export USCRIPT=$USCRIPT1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $USCRIPT --name \"Urlscript\" >> all && exit"
+su - userjenkins -c "echo \"export SECGRPID=$SECGRPID1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $SECGRPID --name \"SecurityGroupId\" >> all && exit"
+su - userjenkins -c "echo \"export INSTTYPE=$INSTTYPE1\" >> ~.bashrc"
+#su - userjenkins -c "cd /home/userjenkins && ansible-vault encrypt_string $INSTTYPE --name \"InstanceType\" >> all && exit"
+#su - userjenkins -c "cd /home/userjenkins && cat /home/userjenkins/all >> /home/userjenkins/depot_projet_1_file_rouge/infra/ansible/inventory/dev/group_vars/all/all"
+#su - userjenkins -c "cd /home/userjenkins && cat /home/userjenkins/all >> /home/userjenkins/depot_projet_1_file_rouge/infra/ansible/inventory/qua/group_vars/all/all"
+#su - userjenkins -c "cd /home/userjenkins && cat /home/userjenkins/all >> /home/userjenkins/depot_projet_1_file_rouge/infra/ansible/inventory/prod/group_vars/all/all"
 
+#Install docker
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl start docker
+#Install docker compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 sleep 30
 # Mdp jenkins
