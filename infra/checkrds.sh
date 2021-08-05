360c003fa46157ebf6206cd468d1e97457cfcb78
gb-0001 <gb-0001@outlook.fr>
2021-08-05 14:19:38 +0200

    add pipeline qua prod

--- infra/checkrds.sh
+++ infra/checkrds.sh
@@ -1,4 +1,5 @@
 #!/bin/bash +xe
+#RECUPERATION DU DNS PUBLIC RDS BDD
 LINES=$(aws rds describe-db-instances --region $1 --query "DBInstances[*].[ [[TagList[?Key==\`aws:cloudformation:stack-name\`].Value]],[Endpoint.Address] ]" | grep "\"" | sed 's/[ |\"]//g')
 SAVEIFS=$IFS   # Save current IFS
 IFS=$'\n'      # Change IFS to new line
