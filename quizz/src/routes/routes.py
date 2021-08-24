4dffc4a76e678bf714f1ffac0606ef83a97da91e
KevinGit31 <77724639+KevinGit31@users.noreply.github.com>
2021-08-24 10:43:14 +0200

    Update routes.py
    add user to list

--- quizz/src/routes/routes.py
+++ quizz/src/routes/routes.py
@@ -8,21 +8,12 @@ menu_list = [
     {"name": "Sujet", "isActive": "", "url": "subject"},
     {"name": "Question", "isActive": "", "url": "question"},
     {"name": "Réponse", "isActive": "", "url": "answer"},
-    {"name": "Utilisateur", "isActive": "active", "url": "user"}
+    {"name": "Utilisateur", "isActive": "", "url": "user"}
 ]
 
 
    @app.route('/')
    def index():
 
-      # Liste des menus
-      menu_list=[
-         {"name":"Accueil","isActive":"active","url":"index"},
-         {"name":"Jouer","isActive":"","url":"game"},
-         {"name":"Mode","isActive":"","url":"mode"},
-         {"name":"Sujet","isActive":"","url":"subject"},
-         {"name":"Question","isActive":"","url":"question"},
-         {"name":"Réponse","isActive":"","url":"answer"}
-      ]
       app.logger.info('index.html')
       return render_template('index.html',menu_list=menu_list )
