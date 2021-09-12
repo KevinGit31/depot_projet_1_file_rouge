bdfd469ffd4d3854eece92a6272718f1bec7a4fe
Gerard Fevill <nouglozeh_gerard@yahoo.fr>
2021-09-12 17:00:25 +0200

    next

--- quizz/client_server/app.js
+++ quizz/client_server/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
+const cors = require('cors');
 
 app.use(express.static(__dirname + '/quizz'));
 
 router.get('*', function(req, res){
   res.sendFile(path.join(__dirname+'/quizz/index.html'));
 });
+
+
+app.use(cors({
+    origin: '*'
+}));
 
 //add the router
 app.use('/', router);
